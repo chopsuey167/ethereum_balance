edc9db6d644855cb49f263b2231b863cfa819015
Ricardo Gutierrez <ricardog.astocondor@gmail.com>
2024-08-21 18:09:13 +0200

    adding new exception

--- src/main/java/com/rga/ethereum_balance/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rga/ethereum_balance/rest/exception/GlobalExceptionHandler.java
@@ -6,12 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.web3j.exceptions.MessageDecodingException;
+import org.web3j.protocol.exceptions.ClientConnectionException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MessageDecodingException.class)
   public ResponseEntity<?> handleMessageDecodingException(MessageDecodingException e) {
+    ErrorDto errorDto = ErrorDto.builder().status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .message(e.getMessage()).build();
+
+    return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(ClientConnectionException.class)
+  public ResponseEntity<?> handleClientConnectionException(ClientConnectionException e) {
     ErrorDto errorDto = ErrorDto.builder().status(HttpStatus.INTERNAL_SERVER_ERROR)
         .message(e.getMessage()).build();
 
